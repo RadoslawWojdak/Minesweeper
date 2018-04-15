537d1dab7f3701281c4421d86bf4f0c46a4226a3
RadoslawWojdak <radoslaw-wojdak@wp.pl>
2018-04-15 03:23:46 +0200

    Checking square status

--- Minesweeper/Square.cpp
+++ Minesweeper/Square.cpp
@@ -18,4 +18,60 @@ cSquare::cSquare(unsigned short x, unsigned short y)
 	);
 
 	_status = unrevealed;
+	_bomb = false;
+}
+
+void cSquare::displayStatus()
+{
+	switch (_status)
+	{
+	case unrevealed:
+		setFillColor(sf::Color(255, 255, 255));
+		break;
+	case revealed:
+	{
+		if (_bomb)
+			setFillColor(sf::Color(0, 0, 0));
+		else
+			setFillColor(sf::Color(0, 0, 255));
+		break;
+	}
+	case flagged:
+		setFillColor(sf::Color(255, 0, 0));
+		break;
+	case questioned:
+		setFillColor(sf::Color(0, 255, 0));
+		break;
+	}
+}
+
+void cSquare::click()
+{
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+	{
+		if (_status != flagged)
+			_status = revealed;
+	}
+	else if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Right))
+	{
+		if (_status != revealed)
+		{
+			if (_status == flagged)
+				_status = unrevealed;
+			else
+				_status = flagged;
+		}
+	}
+	else if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Middle))
+	{
+		if (_status != revealed && _status != flagged)
+		{
+			if (_status == questioned)
+				_status = unrevealed;
+			else
+				_status = questioned;
+		}
+	}
+
+	displayStatus();
 }
--- Minesweeper/Square.h
+++ Minesweeper/Square.h
@@ -5,7 +5,12 @@
 class cSquare :public sf::RectangleShape
 {
 	enum eStatus { unrevealed, revealed, flagged, questioned } _status;
+	bool _bomb;
+
+	void displayStatus();
 
 public:
 	cSquare(unsigned short x, unsigned short y);
+
+	void click();
 };
